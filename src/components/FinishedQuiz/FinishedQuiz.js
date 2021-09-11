60c778094c60bdbfdb5a9d730e41f8667c03297d
Bobeshmo <bobeshmo@gmail.com>
2021-09-11 16:39:48 +0300

    End of the project

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,54 +1,51 @@
 import React from 'react'
 import classes from './FinishedQuiz.css'
-import Button from '../UI/Button/Button';
+import Button from '../UI/Button/Button'
 import {Link} from 'react-router-dom'
 
-class FinishedQuiz extends React.Component {
-    constructor (props) {
-        super(props);
-        this.mySpan = document.getElementsByTagName("span");
-        this.successCount = Object.keys(props.results).reduce((total, key) => {
-            if(props.results[key] === 'success') {
-                total++;
-            }
-            console.log(key);
-            return total;
-        }, 0)
+const FinishedQuiz = props => {
+  const successCount = Object.keys(props.results).reduce((total, key) => {
+    if (props.results[key] === 'success') {
+      total++
     }
 
-    render () {
-        return (
-            <div className={classes.FinishedQuiz}>
-                <ul>
-                    {this.props.quiz.map((quizItem, index) => {
-                        return (
-                            <li key={index}>
-                                <strong>{index + 1}</strong>.&nbsp;
-                                {quizItem.question}
-                                <span className={classes[this.props.results[quizItem.id]]}>
-                                    {
-                                        this.props.results[quizItem.id] === 'error'
-                                        ? this.mySpan.innerHTML = '×'
-                                        : this.mySpan.innerHTML = '√'
-                                    }
-                                </span>
-                            </li>
-                        )
+    return total
+  }, 0)
 
-                    })}
-                </ul>
 
-                <p>Правильно {this.successCount} из {this.props.quiz.length}</p>
+  return (
+    <div className={classes.FinishedQuiz}>
+      <ul>
+        { props.quiz.map((quizItem, index) => {
+          const cls = [
+            'fa',
+            props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+            classes[props.results[quizItem.id]]
+          ]
 
-                <div>
-                    <Button onClick={this.props.onRetry} type='primary'>Повторить</Button>
-                    <Link to={'/'}>
-                        <Button type='success'>Перейти в список тестов</Button>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+          return (
+            <li
+              key={index}
+            >
+              <strong>{index + 1}</strong>.&nbsp;
+              {quizItem.question}
+              <i className={cls.join(' ')} />
+            </li>
+          )
+
+        }) }
+      </ul>
+
+      <p>Правильно {successCount} из {props.quiz.length}</p>
+
+      <div>
+        <Button onClick={props.onRetry} type="primary">Повторить</Button>
+        <Link to="/">
+          <Button type="success">Перейти в список тестов</Button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default FinishedQuiz
